6e2a3aaad4abe7360b45db2f555fc472b7dd4a3e
EricSeoungminKim <seoungminkim03@g.ucla.edu>
2023-02-14 02:04:59 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="website-overall">
+      <header className="website-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="website-links"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Test Link
         </a>
       </header>
     </div>
